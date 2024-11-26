dd4233768e408267649ac0e8dbb36760007c0c0e
Fernando Belo <fernandonfb20@gmail.com>
2024-11-25 21:28:58 -0300

    :sparkles: Initial FrontEnd

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+export default function App() {
+
+  return (
+    <>
+     <h1 style={{background: 'red', width:'fit-content', padding:'200px'}}>My Gastronomy</h1>
+     <p>Testando React </p>
+    </>
+  )
+}
+
+
